6ba1b089b8889384ff475556bcfa20204c4c6847
Daniel Kehlet <daniel_kehlet@hotmail.com>
2022-05-06 11:44:36 +0200

    Inserts society and changes to Inserts(key)

--- Inserts.js
+++ Inserts.js
@@ -1,12 +1,4 @@
-db.key.insertOne({
+db.Municipality.insertOne({
+	Type: "Key",
 	LocationID:1
 })
-
-db.Member.insertMany([
-{item: "Andreas", Name: "Andreas", Address: "Mejlgade 103", Cpr: 190298},
-{item: "Daniel", Name: "Daniel", Address: "Mejlgade 102", Cpr: 190297}])
-
-
-
-
-
