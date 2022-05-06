0a1bc36d55ef0e23d69682dd1446bd2f077e06ef
Farcai <farcait@gmail.com>
2022-05-06 11:40:24 +0200

    Create TimeSlot.js
    Inserts TimeSlots

---
+++ TimeSlot.js
@@ -0,0 +1,31 @@
+/*db.Municipality.insertMany([
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 0, 0, 0), To: new Date(2022, 4, 6, 1, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 1, 0, 0), To: new Date(2022, 4, 6, 2, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 2, 0, 0), To: new Date(2022, 4, 6, 3, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 3, 0, 0), To: new Date(2022, 4, 6, 4, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 4, 0, 0), To: new Date(2022, 4, 6, 5, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 5, 0, 0), To: new Date(2022, 4, 6, 6, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 6, 0, 0), To: new Date(2022, 4, 6, 7, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 7, 0, 0), To: new Date(2022, 4, 6, 8, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 8, 0, 0), To: new Date(2022, 4, 6, 9, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 9, 0, 0), To: new Date(2022, 4, 6, 10, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 10, 0, 0), To: new Date(2022, 4, 6, 11, 0, 0)},
+{Type: "TimeSlot", From: new Date(2022, 4, 6, 11, 0, 0), To: new Date(2022, 4, 6, 12, 0, 0)}
+])*/
+
+db.Municipality.insertMany([
+{Type: "TimeSlot", From: new Date("2022-05-06"), To: new Date("2022-05-07")},
+{Type: "TimeSlot", From: new Date("2022-05-07"), To: new Date("2022-05-08")},
+{Type: "TimeSlot", From: new Date("2022-05-08"), To: new Date("2022-05-09")},
+{Type: "TimeSlot", From: new Date("2022-05-09"), To: new Date("2022-05-10")},
+{Type: "TimeSlot", From: new Date("2022-05-10"), To: new Date("2022-05-11")},
+{Type: "TimeSlot", From: new Date("2022-05-11"), To: new Date("2022-05-12")},
+{Type: "TimeSlot", From: new Date("2022-05-12"), To: new Date("2022-05-13")},
+{Type: "TimeSlot", From: new Date("2022-05-13"), To: new Date("2022-05-14")},
+{Type: "TimeSlot", From: new Date("2022-05-14"), To: new Date("2022-05-15")},
+{Type: "TimeSlot", From: new Date("2022-05-15"), To: new Date("2022-05-16")},
+{Type: "TimeSlot", From: new Date("2022-05-16"), To: new Date("2022-05-17")},
+{Type: "TimeSlot", From: new Date("2022-05-17"), To: new Date("2022-05-18")},
+{Type: "TimeSlot", From: new Date("2022-05-18"), To: new Date("2022-05-19")},
+{Type: "TimeSlot", From: new Date("2022-05-19"), To: new Date("2022-05-20")}
+])
